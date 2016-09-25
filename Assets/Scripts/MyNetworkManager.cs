6d2d03b9810fd1eab1ca2757c0b535d83e6cb145
Andrew Bell <fiixed@gmail.com>
2016-09-25 16:50:27 -0500

    added custom NetworkManager script

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost() {
+		Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+		GetComponent<NetworkManager>();
+		StartHost();
+		// string theTime = System.DateTime.Now.ToString();
+	}
+
+	public override void OnStartHost() {
+		Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+	}
+}
