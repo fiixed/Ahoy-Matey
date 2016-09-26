ecc64a69065097ccfacc6bba2fb00d8c236b28b5
Andrew Bell <fiixed@gmail.com>
2016-09-26 17:13:57 -0500

    added local movement

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost() {
+		Debug.Log("Starting Host. " + Time.timeSinceLevelLoad);
+		GetComponent<NetworkManager>();
+		StartHost();
+		// string theTime = System.DateTime.Now.ToString();
+	}
+
+	public override void OnStartHost() {
+		Debug.Log("Host started. " + Time.timeSinceLevelLoad);
+	}
+
+	public override void OnStartClient(NetworkClient myClient) {
+		Debug.Log("Client start requested. " + Time.timeSinceLevelLoad);
+		InvokeRepeating("PrintDots", 0f, 1f);
+		
+	}
+
+	public override void OnClientConnect(NetworkConnection conn) {
+		Debug.Log("Client is connected to IP: " + conn.address + " at. " + Time.timeSinceLevelLoad);
+		CancelInvoke();
+	}
+
+	void PrintDots() {
+		Debug.Log(".");
+	}
+}
